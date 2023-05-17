376d297fffabd9540300254a1f8c7b276a44c6e0
410 <58301450+410-dev@users.noreply.github.com>
2023-05-17 03:47:51 -0400

    Removed prints

--- data/commands/atlas/main.py
+++ data/commands/atlas/main.py
@@ -27,37 +27,30 @@ class Atlas:
     def main(self) -> int:
         
         # Check if installation was successful
-        print("Verifying installation...")
         if Registry.read("SOFTWARE.Helium.Program.Atlas.SetupDone") != "1":
             print(Atlas.error(1))
             return 1
-        print("Installation verified.")
             
         # Check running mode
         # If use offline model, go to offline().
         # If use online api, go to online().
-        print("Validating configuration...")
         import data.commands.atlas.validate as Validator
         offlineMode = Registry.read("SOFTWARE.Helium.Program.Atlas.Local.UseOfflineModel")
         if offlineMode == "1":
-            print("Offline mode!")
             exitCode = Validator.offline()
         else:
-            print("Online mode!")
             exitCode = Validator.online()
         
         # If validation failed, return exit code.
         if exitCode != 0 and not None:
-            print(f"Validator returned signal {exitCode}")
             return exitCode
-        print("Configuration validated.")
 
         if offlineMode == "1":
             import data.commands.atlas.offline as llm
         else:
             import data.commands.atlas.openai as llm
         
-        llm.interactiveMode()
+        return llm.interactiveMode(self.args)
         
         
         
