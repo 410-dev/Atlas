22c958d86a8012a9920e66f88d3126fb51e3c2fb
410 <58301450+410-dev@users.noreply.github.com>
2023-05-18 02:49:59 -0400

    Added llmachcontrolstage

---
+++ data/commands/atlas-llmach-control-stage/main.py
@@ -0,0 +1,9 @@
+
+# Atlas Low Level Machine Control Stage
+class AtlasLlmachControlStage:
+    
+    def __init__(self, lineArgs) -> None:
+        self.args: list = lineArgs
+        
+    def main(self) -> int:
+        return 0
