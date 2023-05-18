2ce3240f949af0f8fb46663031a3bf4a50da1d54
410 <58301450+410-dev@users.noreply.github.com>
2023-05-18 02:05:14 -0400

    Hook changed to new format

--- data/hooks/atlas.py
+++ data/hooks/atlas.py
@@ -2,18 +2,14 @@ from kernel.states import States
 import kernel.registry as Registry
 import kernel.procmgr as procmgr
 
-class atlas():
-    
-    def __init__(self, args):
-        self.args = args
-    
-    def main(self) -> int:
-        if Registry.read("SOFTWARE.Helium.Program.Atlas.SetupDone") == "1":
-            if Registry.read("SOFTWARE.Helium.Program.Atlas.Local.LoadModelOnSystemBoot") == "1":
-                modelName = Registry.read("SOFTWARE.Helium.Program.Atlas.ModelSelected")
-                procmgr.launch("atlas-model", ["load", modelName])
-            else:
-                print("Model is not loaded on system boot.")
+
+def main(args) -> int:
+    if Registry.read("SOFTWARE.Helium.Program.Atlas.SetupDone") == "1":
+        if Registry.read("SOFTWARE.Helium.Program.Atlas.Local.LoadModelOnSystemBoot") == "1":
+            modelName = Registry.read("SOFTWARE.Helium.Program.Atlas.ModelSelected")
+            procmgr.launch("atlas-model", ["load", modelName])
         else:
-            print("[Atlas Hook]: Atlas is not configured yet. Please run atlas-install.")
+            print("Model is not loaded on system boot.")
+    else:
+        print("[Atlas Hook]: Atlas is not configured yet. Please run atlas-install.")
     
